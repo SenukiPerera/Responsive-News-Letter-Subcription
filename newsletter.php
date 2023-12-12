378b5cf2dd00e53fa2836f7c7dc3ebba1d51fb82
Senuki Perera <109462298+SenukiPerera@users.noreply.github.com>
2023-12-12 23:05:53 +0530

    update email

--- newsletter.php
+++ newsletter.php
@@ -11,25 +11,6 @@
                     <h3>Subscribe our News Letter</h3>
                     <form action="newsletter.php" method="POST">
                         <?php
-                        /*$userEmail = "";
-                        if(isset($_POST['subscribe']))//if subscribe button clicked
-                        { 
-                            $userEmail = $_POST['email']; //getting user email
-                            if(filter_var($userEmail, FILTER_VALIDATE_EMAIL)){ //validating user entered email
-                                //echo "Email is correct";
-                                $subject = "Thanks for Subscribing us - Job Seeker";
-                                $message = "Thanks for Subscribing Us. You'll always receive latest updates from us. And we won't share or sell your information to anyone.";
-                                $sender = "From: pereraiasm@gmail.com";
-                                if(mail($userEmail, $subject, $message, $sender))
-                                {
-                                    echo "Thanks for subscribing us!";
-                                }
-                            }else{
-                               
-                                echo "Failed while sending your email!";
-                                
-                            }
-                        }*/
                         if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
                         
@@ -37,7 +18,7 @@
                                 $to = $email;
                                 $subject = "Thanks for Subscribing us - Job Seeker";
                                 $message = "Thanks for Subscribing Us. You'll always receive latest updates from us. And we won't share or sell your information to anyone.";
-                                $sender = "From: pereraiasm@gmail.com";
+                                $sender = "From: /*Your email here*/";
                                 if(mail($to, $subject, $message, $sender))
                                 {
                                     echo "Thank you for subscribing!";
